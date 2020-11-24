e2a77ef72b70875c895537450a04d8486f2d9857
Piotr <piotrek01218@o2.pl>
2020-11-24 12:40:31 +0100

    Ustawienia wybór sensora

--- Wyscig/app/src/main/java/com/example/wyscig/Ustawienia.java
+++ Wyscig/app/src/main/java/com/example/wyscig/Ustawienia.java
@@ -3,9 +3,11 @@ package com.example.wyscig;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ public class Ustawienia extends AppCompatActivity {
     Button wyjscie, zapis;
     EditText szerokosc, wysokosc;
     int obwod;
+    RadioButton jasnosc, odleglosc;
 
     public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,14 @@ public class Ustawienia extends AppCompatActivity {
 
         zapis = findViewById(R.id.zapis);
         zapis.setOnClickListener(v -> Pole());
+
+        SharedPreferences ustawienia = getSharedPreferences("ustawienia", MODE_PRIVATE);
+        try {
+            jasnosc = (RadioButton) findViewById(R.id.jasnosc);
+            odleglosc = (RadioButton) findViewById(R.id.odleglosc);
+            jasnosc.setChecked(ustawienia.getBoolean("ustawienia_jasnosc",true));
+            odleglosc.setChecked(ustawienia.getBoolean("ustawienia_odleglosc", false));
+        } catch (Throwable e){}
     }
     public void Pole(){
         try {
@@ -36,7 +47,7 @@ public class Ustawienia extends AppCompatActivity {
             String szerStr = szerokosc.getText().toString();
             int szerInt = Integer.parseInt(szerStr);
 
-            obwod = 2*(wysInt + szerInt + 20);
+            obwod = 2*(wysInt + szerInt + 40);
 
             final TextView textViewToChange = (TextView) findViewById(R.id.obwodText);
             textViewToChange.setText("Obwód to: "+ obwod);
@@ -54,7 +65,17 @@ public class Ustawienia extends AppCompatActivity {
         }
 
     }
+    public void onRadioButtonClicked(View v) {
+        jasnosc = (RadioButton) findViewById(R.id.jasnosc);
+        odleglosc = (RadioButton) findViewById(R.id.odleglosc);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("ustawienia", MODE_PRIVATE);
+        SharedPreferences.Editor ustawienia = sharedPreferences.edit();
+
+        ustawienia.putBoolean("ustawienia_jasnosc", jasnosc.isChecked());
+        ustawienia.putBoolean("ustawienia_odleglosc", odleglosc.isChecked());
+        ustawienia.commit();
+    }
     public void openMain() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);

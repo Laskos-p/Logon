0f5c75e7675c2d428342b8753c50942882793278
giro21 <leszy21@gmail.com>
2020-12-07 10:16:57 +0100

    Wyniki v1

--- Wyscig/app/src/main/java/com/example/wyscig/Wyniki.java
+++ Wyscig/app/src/main/java/com/example/wyscig/Wyniki.java
@@ -4,22 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Wyniki extends AppCompatActivity {
+    private String dataS, tempoS, czas;
+    private int okrazenia;
+    private float trasa, predkosc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wyniki);
-
-/*
         SharedPreferences wyniki = getSharedPreferences("wyniki", MODE_PRIVATE);
-        wyniki.putString("data", dataS);
-        wyniki.putInt("okrazenia", okrazenia);
-        wyniki.putFloat("dystans", (float) trasa);
-        wyniki.putFloat("predkosc", (float) predkosc);
-        wyniki.putString("tempo", TempoS);
-        wyniki.putString("czas", getString(R.string.czas, hours, minutes, seconds));
-        */
+        dataS = wyniki.getString("data", null);
+        okrazenia = wyniki.getInt("okrazenia", 0);
+        trasa = wyniki.getFloat("dystans", 0);
+        predkosc = wyniki.getFloat("predkosc", 0);
+        tempoS = wyniki.getString("tempo", null);
+        czas = wyniki.getString("czas", null);
+
+        final TextView textViewToChange = (TextView) findViewById(R.id.test_text);
+        textViewToChange.setText("Podaj prawidłowe wartości");
+
     }
 }

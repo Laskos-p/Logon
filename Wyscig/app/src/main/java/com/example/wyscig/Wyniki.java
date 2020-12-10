7f56eb7c3a01daf94963e0d0bf5d6fe18549122b
Adrian-dotcom <74772343+Adrian-dotcom@users.noreply.github.com>
2020-12-10 13:52:22 +0100

    Zaokrąglanie prędkości w wynikach

--- Wyscig/app/src/main/java/com/example/wyscig/Wyniki.java
+++ Wyscig/app/src/main/java/com/example/wyscig/Wyniki.java
@@ -17,12 +17,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.DecimalFormat;
 
 public class Wyniki extends AppCompatActivity {
-    private String dataS, tempoS, czas, wyniki_zapis, wyniki_zapis1;
+    private String dataS, tempoS, czas, wyniki_zapis, wyniki_zapis1, predkoscS;
     private int okrazenia;
     private float trasa, predkosc;
     Button powrot, resett;
+    DecimalFormat precision2 = new DecimalFormat("0.00");
 
 
     @Override
@@ -38,7 +40,7 @@ public class Wyniki extends AppCompatActivity {
         tempoS = wyniki.getString("tempo", null);
         czas = wyniki.getString("czas", null);
 
-        wyniki_zapis = "\n"+ dataS + "\nOkrążeń: " +okrazenia+ "\nTrasa: " +trasa+ "m\nPrędkość: " +predkosc+ "km/h\n" +tempoS+ "\nCzas: " +czas+ "\n________________________\n";
+        wyniki_zapis = "\n" + dataS + "\nOkrążeń: " + okrazenia + "\nTrasa: " + trasa + "m\nPrędkość: " + precision2.format(predkosc) + "km/h\n" + tempoS + "\nCzas: " + czas + "\n________________________\n";
 
         resett = findViewById(R.id.reset);
         resett.setOnClickListener(v -> {
@@ -57,14 +59,14 @@ public class Wyniki extends AppCompatActivity {
         powrot.setOnClickListener(v -> openMainActivity());
 
 
-
         final TextView textViewToChange = (TextView) findViewById(R.id.test_text);
         //textViewToChange.setText(dataS + " " +okrazenia+ " " +trasa+ "m " +predkosc+ "km/h " +tempoS+ "m/km " +czas+"s");
         textViewToChange.setText(readFromFile(this));
         //textViewToChange.setText(wyniki_zapis);
 
     }
-    public void openMainActivity(){
+
+    public void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         this.finish();
@@ -76,11 +78,11 @@ public class Wyniki extends AppCompatActivity {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
             outputStreamWriter.write(data);
             outputStreamWriter.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Log.e("Exception", "Zapis się nie powiódł: " + e.toString());
         }
     }
+
     //ODCZYT
     private String readFromFile(Context context) {
 
@@ -89,21 +91,20 @@ public class Wyniki extends AppCompatActivity {
         try {
             InputStream inputStream = context.openFileInput("config.txt");
 
-            if ( inputStream != null ) {
+            if (inputStream != null) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 String receiveString = "";
                 StringBuilder stringBuilder = new StringBuilder();
 
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                while ((receiveString = bufferedReader.readLine()) != null) {
                     stringBuilder.append("\n").append(receiveString);
                 }
 
                 inputStream.close();
                 ret = stringBuilder.toString();
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
             Log.e("login activity", "Can not read file: " + e.toString());

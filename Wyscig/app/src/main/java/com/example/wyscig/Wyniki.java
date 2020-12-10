212a77c5809e32013228fd9bec78d0cb4dd52fa1
giro21 <leszy21@gmail.com>
2020-12-10 12:45:20 +0100

    historia reset v3

--- Wyscig/app/src/main/java/com/example/wyscig/Wyniki.java
+++ Wyscig/app/src/main/java/com/example/wyscig/Wyniki.java
@@ -40,7 +40,10 @@ public class Wyniki extends AppCompatActivity {
         wyniki_zapis = "\n"+ dataS + "\nOkrążeń: " +okrazenia+ "\nTrasa: " +trasa+ "m\nPrędkość: " +predkosc+ "km/h\n" +tempoS+ "\nCzas: " +czas+ "\n________________________\n";
 
         resett = findViewById(R.id.reset);
-        resett.setOnClickListener(v -> wyniki_zapis1 = null);
+        resett.setOnClickListener(v -> {
+            wyniki_zapis1 = null;
+            writeToFile(wyniki_zapis1, this);
+        });
 
         wyniki_zapis1 = readFromFile((this)) + wyniki_zapis;
         writeToFile(wyniki_zapis1, this);
